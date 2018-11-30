68e7e085e30d5b8da857865ce5eb823e56dbf77e
Chiboba <briangfelix@gmail.com>
2018-11-30 10:11:02 -0800

    MVP

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,9 +1,12 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
 1. What is the purpose of using _sessions_?
-
+    Sessions act as a way to store information for a user across multiple pages. Sessions also allow us to control access to the site. Whether it be how long someone has access or what they can see.
 2. What does bcrypt do to help us store passwords in a secure manner.
-
+ Bcrypt hashes the password so if a hacker somehow gains access to the password information, they can't just use that to get into the users account they would need to compare countless passwords until they finally get a match to the bcrypt hash.
 3. What does bcrypt do to slow down attackers?
-
+ It both hashes and is computationaly slow so hackers can't just test large amounts against the hash as often as they would with other hashing systems.
 4. What are the three parts of the JSON Web Token?
+header, payload, signature
+
+Using jsonwebtokens we provide: payload(first, third) and signature(second)
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../database/dbConfig')
+const secret = require('../_secrets/keys')
 
 const { authenticate } = require('./middlewares');
 
@@ -8,18 +12,46 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken() {
+
+}
+
 function register(req, res) {
-  // implement user registration
+  const {username, password} = req.body;
+  bcrypt.hash(password, 8)
+  .then(password => 
+    db('users').insert({username, password}))
+  .then((id) => {
+    res.status(201).json(id)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
 }
 
 function login(req, res) {
-  // implement user login
+  const user = req.body;
+  db('users')
+  .where('username', '=', user.username)
+  .first()
+  .then(stored => {
+    if(user && bcrypt.compareSync(user.password, stored.password)) {
+      console.log(secret)
+      const token = jwt.sign({username: stored.username}, secret.jwtKey, { expiresIn: '24h' });
+      res.status(200).json(token);
+    } else {
+      res.status(406).json({message:'Authentication Error'})
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
 }
 
 function getJokes(req, res) {
   axios
     .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
+      'https://safe-falls-22549.herokuapp.com/random_ten'
     )
     .then(response => {
       res.status(200).json(response.data);
